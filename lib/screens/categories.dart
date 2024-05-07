8ae6efabe6590dcc3c9a239ad8ff20428be2cbce
Ogoigbe1 <ogoigbe12@gmail.com>
2024-05-07 13:44:16 +0100

    "Add dummy meals data and navigation to MealsScreen from CategoriesScreen"
    This diff adds a new list of dummy meal data and updates the CategoriesScreen to navigate to the MealsScreen when a category is selected. The CategoryGrid

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (ctx) => MealsScreen(
+            title: 'Some title',
+            meals: [],
+          ),
+        )); // Navigator.of(context).push(route) also work too
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +24,19 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [ 
-          Text('1',style: TextStyle(color: Colors.white)),
-          Text('2',style: TextStyle(color: Colors.white)),
-          Text('3',style: TextStyle(color: Colors.white)),
-          Text('4',style: TextStyle(color: Colors.white)),
-          Text('5',style: TextStyle(color: Colors.white)),
-          Text('6',style: TextStyle(color: Colors.white)),
-
+        children: [
+          // availableCategories.map((category)=>CategoryGridItem(category: category)).toList() sub for using for loop
+          for (final category in availableCategories)
+            CategoryGridItem(category: category, onSelectCategory: () {
+              _selectCategory(context);
+            },)
         ],
       ),
     );
